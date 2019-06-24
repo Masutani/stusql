84adc523acc46dbc6bcedc894587bae0cc889163
Osamu Masutani <masutani_itlab@live.jp>
2019-06-24 17:51:50 +0900

    Distance calculation applier added. Geohash applier added on GeoOperator .

---
+++ USQLSharedLib/DistApplier.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Analytics.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Device.Location;
+
+namespace USQLSharedLib
+{
+    [SqlUserDefinedApplier]
+    public class DistApplier : IApplier
+    {
+        private string lat1Column;
+        private string lon1Column;
+        private string lat2Column;
+        private string lon2Column;
+        private int type;
+
+        public DistApplier(string lat1Column, string lon1Column, string lat2Column, string lon2Column, int type)
+        {
+            this.lat1Column = lat1Column;
+            this.lon1Column = lon1Column;
+            this.lat2Column = lat2Column;
+            this.lon2Column = lon2Column;
+            this.type = type;
+        }
+        public override IEnumerable<IRow> Apply(IRow input, IUpdatableRow output)
+        {
+            Nullable<double> lat1 = input.Get<Nullable<double>>(lat1Column);
+            Nullable<double> lon1 = input.Get<Nullable<double>>(lon1Column);
+            Nullable<double> lat2 = input.Get<Nullable<double>>(lat2Column);
+            Nullable<double> lon2 = input.Get<Nullable<double>>(lon2Column);
+
+            var lat1n = lat1 ?? 0;
+            var lon1n = lon1 ?? 0;
+            var lat2n = lat2 ?? 0;
+            var lon2n = lon2 ?? 0;
+            if( (lat1n == 0 & lon1n == 0) | (lat2n == 0 & lon2n == 0))
+            {
+                output.Set<double>("distance", 0);
+                yield return output.AsReadOnly();
+            }
+
+            double distance = 0;
+            if (type == 1)
+            {
+                distance = harversineDist1(lat1n, lon1n, lat2n, lon2n);
+            }
+            else if (type == 2)
+            {
+                distance = harversineDist2(lat1n, lon1n, lat2n, lon2n);
+            }
+            else if (type == 3)
+            {
+                distance = harversineDist3(lat1n, lon1n, lat2n, lon2n);
+            }
+            output.Set<double>("distance", distance);
+
+            yield return output.AsReadOnly();
+        }
+        
+        public static double harversineDist1(double lat1, double lon1, double lat2, double lon2)
+        {
+            double R = 6371; // km
+            double sLat1 = Math.Sin(Radians(lat1));
+            double sLat2 = Math.Sin(Radians(lat2));
+            double cLat1 = Math.Cos(Radians(lat1));
+            double cLat2 = Math.Cos(Radians(lat2));
+            double cLon = Math.Cos(Radians(lon1) - Radians(lon2));
+            double cosD = sLat1 * sLat2 + cLat1 * cLat2 * cLon;
+            double d = Math.Acos(cosD);
+            double dist = R * d;
+            return dist;
+        }
+        public static double Radians(double degrees)
+        {
+            double radians = (Math.PI / 180) * degrees;
+            return (radians);
+        }
+
+        public static double harversineDist2(double lon1, double lat1, double lon2, double lat2)
+        {
+            const double r = 6371; // meters
+            var sdlat = Math.Sin((lat2 - lat1) / 2);
+            var sdlon = Math.Sin((lon2 - lon1) / 2);
+            var q = sdlat * sdlat + Math.Cos(lat1) * Math.Cos(lat2) * sdlon * sdlon;
+            double d = 2 * r * Math.Asin(Math.Sqrt(q));
+            return d;
+        }
+        public static double harversineDist3(double lon1, double lat1, double lon2, double lat2)
+        {
+            GeoCoordinate c1 = new GeoCoordinate(lat1, lon1);
+            GeoCoordinate c2 = new GeoCoordinate(lat2, lon2);
+
+            double distanceInKm = c1.GetDistanceTo(c2) / 1000;
+            return distanceInKm;
+        }
+    }
+}
--- USQLSharedLib/GeoOperator.cs
+++ USQLSharedLib/GeoOperator.cs
@@ -1,22 +1,24 @@
 ﻿using Microsoft.Analytics.Interfaces;
-using Microsoft.Analytics.Types.Sql;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Spatial;
 using Wibci.CountryReverseGeocode;
 using Wibci.CountryReverseGeocode.Models;
 
 namespace USQLSharedLib
 {
     [SqlUserDefinedProcessor]
+
+    // reverse geocder 
+    // using geocoding library : https://github.com/InquisitorJax/Wibci.CountryReverseGeocode
+    // only country and USStates are supported
+    //
     public class ReverseGeocoder : IProcessor
     {
+        #region member variables
         private string latColumn;
         private string lonColumn;
         private CountryReverseGeocodeService _service = new CountryReverseGeocodeService();
+        #endregion
+
         public ReverseGeocoder(string latColumn, string lonColumn)
         {
             this.latColumn = latColumn;
@@ -49,5 +51,167 @@ namespace USQLSharedLib
             return output.AsReadOnly();
         }
     }
+    
+    [SqlUserDefinedProcessor]
+    public class GeoHashDecoder : IProcessor
+    {
+        public override IRow Process(IRow input, IUpdatableRow output)
+        {
+            string geohash7 = input.Get<string>("geohash7");
+            var coord = Geohash.Decode(geohash7);
+            output.Set<string>("geohash7", geohash7);
+            output.Set<double>("lat", coord[0]);
+            output.Set<double>("lon", coord[1]);
+            return output.AsReadOnly();
+        }
+    }
+
+    // inline geo hash decoder helper class
+    // using geohash library : https://github.com/sharonjl/geohash-net
+    //
+    public static class Geohash
+    {
+        #region Direction enum
+        public enum Direction
+        {
+            Top = 0,
+            Right = 1,
+            Bottom = 2,
+            Left = 3
+        }
+        #endregion
+
+        #region consts list
+        private const string Base32 = "0123456789bcdefghjkmnpqrstuvwxyz";
+        private static readonly int[] Bits = new[] { 16, 8, 4, 2, 1 };
+        private static readonly string[][] Neighbors = {
+                                                           new[]
+                                                               {
+                                                                   "p0r21436x8zb9dcf5h7kjnmqesgutwvy", // Top
+                                                                   "bc01fg45238967deuvhjyznpkmstqrwx", // Right
+                                                                   "14365h7k9dcfesgujnmqp0r2twvyx8zb", // Bottom
+                                                                   "238967debc01fg45kmstqrwxuvhjyznp", // Left
+                                                               }, new[]
+                                                                      {
+                                                                          "bc01fg45238967deuvhjyznpkmstqrwx", // Top
+                                                                          "p0r21436x8zb9dcf5h7kjnmqesgutwvy", // Right
+                                                                          "238967debc01fg45kmstqrwxuvhjyznp", // Bottom
+                                                                          "14365h7k9dcfesgujnmqp0r2twvyx8zb", // Left
+                                                                      }
+                                                       };
+        private static readonly string[][] Borders = {
+                                                         new[] {"prxz", "bcfguvyz", "028b", "0145hjnp"},
+                                                         new[] {"bcfguvyz", "prxz", "0145hjnp", "028b"}
+                                                     };
+        #endregion
+
+        public static String CalculateAdjacent(String hash, Direction direction)
+        {
+            hash = hash.ToLower();
+
+            char lastChr = hash[hash.Length - 1];
+            int type = hash.Length % 2;
+            var dir = (int)direction;
+            string nHash = hash.Substring(0, hash.Length - 1);
+
+            if (Borders[type][dir].IndexOf(lastChr) != -1)
+            {
+                nHash = CalculateAdjacent(nHash, (Direction)dir);
+            }
+            return nHash + Base32[Neighbors[type][dir].IndexOf(lastChr)];
+        }
+
+        public static void RefineInterval(ref double[] interval, int cd, int mask)
+        {
+            if ((cd & mask) != 0)
+            {
+                interval[0] = (interval[0] + interval[1]) / 2;
+            }
+            else
+            {
+                interval[1] = (interval[0] + interval[1]) / 2;
+            }
+        }
+
+        public static double[] Decode(String geohash)
+        {
+            bool even = true;
+            double[] lat = { -90.0, 90.0 };
+            double[] lon = { -180.0, 180.0 };
+
+            foreach (char c in geohash)
+            {
+                int cd = Base32.IndexOf(c);
+                for (int j = 0; j < 5; j++)
+                {
+                    int mask = Bits[j];
+                    if (even)
+                    {
+                        RefineInterval(ref lon, cd, mask);
+                    }
+                    else
+                    {
+                        RefineInterval(ref lat, cd, mask);
+                    }
+                    even = !even;
+                }
+            }
+
+            return new[] { (lat[0] + lat[1]) / 2, (lon[0] + lon[1]) / 2 };
+        }
+
+        public static String Encode(double latitude, double longitude, int precision = 12)
+        {
+            bool even = true;
+            int bit = 0;
+            int ch = 0;
+            string geohash = "";
+
+            double[] lat = { -90.0, 90.0 };
+            double[] lon = { -180.0, 180.0 };
+
+            if (precision < 1 || precision > 20) precision = 12;
+
+            while (geohash.Length < precision)
+            {
+                double mid;
+
+                if (even)
+                {
+                    mid = (lon[0] + lon[1]) / 2;
+                    if (longitude > mid)
+                    {
+                        ch |= Bits[bit];
+                        lon[0] = mid;
+                    }
+                    else
+                        lon[1] = mid;
+                }
+                else
+                {
+                    mid = (lat[0] + lat[1]) / 2;
+                    if (latitude > mid)
+                    {
+                        ch |= Bits[bit];
+                        lat[0] = mid;
+                    }
+                    else
+                        lat[1] = mid;
+                }
+
+                even = !even;
+                if (bit < 4)
+                    bit++;
+                else
+                {
+                    geohash += Base32[ch];
+                    bit = 0;
+                    ch = 0;
+                }
+            }
+            return geohash;
+        }
+    }
+
 
 }
